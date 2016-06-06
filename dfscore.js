47d0fe265ba9187f8c7fecb2ed49aeffa9e7c768
1bpm warez <richard@1bpm.net>
2016-06-06 20:47:53 +0100

    removed npm installing things

--- dfscore.js
+++ dfscore.js
@@ -1,17 +1,3 @@
 #!/usr/bin/env node
 var npm=require("npm");
-
-// attempt to dynamically use npm
-npm.load(function(npmError){
-    if (npmError) throw "There was an error with NPM";
-    console.log("Setting up dependencies");
-    
-    // attempt dynamic (re)install of fibers
-    npm.commands.install(["fibers@1.0.8"],function(installError,data){
-        if (installError) throw "There was an error installing Fibers. Please attempt this manually.";
-        
-        // everything ok, launch the dfscore server
-        console.log("Dependencies satisfied. Launching dfscore server.");
-        require("./lib/server.js").serveForever();
-    });
-});
+require("./lib/server.js").serveForever();
